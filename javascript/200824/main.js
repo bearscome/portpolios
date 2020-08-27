d6196024f75289a4a771a70646fb181cdd79dee0
Hyo_sung <gytjddlsla@gmail.com>
2020-08-27 11:21:49 +0900

    수정과 배움

---
+++ javascript/200824/main.js
@@ -0,0 +1,15 @@
+/*'use strict';
+    를 써야 하는 이유는 변수 선언없이 a=10이라고 적어도 오류 창이 안뜸.
+    하지만 'use strict';를 작성 후에 a=10이라고 쓰면 오류가 뜸 let a; a=10 이렇게 써야함
+
+*/
+/*var, let 
+    let=ES6문법 var 이후에 나온 변수
+    var -> hoisting 어디에 선언했느냐에 상관 없이 선언을 제일 위로 옮기는 것, var blockscope
+    var -> 은 지역변수 전역변수 상관없이 상위로 올라감.
+*/
+
+
+console.log('hellow word')
+
+
